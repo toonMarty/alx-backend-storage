0f55d5c0c987a58eee4befbb812c6d4495eb9bc8
toonMarty <martinnyaga7373@gmail.com>
2023-02-08 22:53:18 +0000

    Old school

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,6 +4,6 @@
 -- This script can be executed on any database
 SELECT band_name, IF(split IS NULL, 2020-formed, split-formed) AS lifespan
 	FROM metal_bands
-	WHERE style LIKE '%Glam rock%'
+	WHERE style LIKE BINARY '%Glam rock%'
 	ORDER BY lifespan DESC;
 
